733884ddd8b5794622d70d20447ee46b0da22bd2
cosme12 <aaadiego502@gmail.com>
2019-11-16 01:04:51 -0300

    Fix signup bug

--- basic/views.py
+++ basic/views.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import get_user_model
+
+
 
 from .models import TodoItem
 
@@ -19,6 +23,7 @@ def office_view(request):
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    User = get_user_model()
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
