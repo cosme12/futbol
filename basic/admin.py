c4b079a299f109a3bf871c53cefa163edbf881b6
cosme12 <aaadiego502@gmail.com>
2019-11-21 17:19:59 -0300

    Add models to admin panel

--- basic/admin.py
+++ basic/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from basic.models import CustomUser, League, Team, Player
+
+#admin.site.site_header = "My Product Inventory "
+#admin.site.site_title = "My Product Inventory "
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'team_id', 'last_login']
+    #list_filter = ('position', )
+    #fields = [('name', 'surname'), ]  # when editting
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'surname', 'position', 'skill_current', 'skill_max', 'salary']
+    list_filter = ('position', )
+    #fields = [('name', 'surname'), ]  # when editting
+
+#admin.site.register(CustomUser)
+admin.site.register(League)
+admin.site.register(Team)
+#admin.site.register(Player, PlayerAdmin)
